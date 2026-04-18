44349b95e6498b2b15304b196b802ffb52907d2a
Sahil Kourav <sahil-kourav@users.noreply.github.com>
2026-04-18 15:57:58 +0530

    Remove localhost from CORS origin configuration for production environment

--- server/cart_service/src/app.js
+++ server/cart_service/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser())
 
 app.use(cors(
     {
-        origin: ['http://localhost:3000', 'https://evercart-delta.vercel.app'],
+        origin: 'https://evercart-delta.vercel.app',
         credentials: true,
     }
 ));
