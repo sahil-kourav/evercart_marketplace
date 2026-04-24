3247cfaa4a47963875b7d4e785464a573be0f9f6
Sahil Kourav <sahil-kourav@users.noreply.github.com>
2026-04-24 13:27:03 +0530

    Refactor R/ootAdminLayout for improved loading state management and authentication check

--- client/app/admin/(admin)/layout.jsx
+++ client/app/admin/(admin)/layout.jsx
@@ -1,76 +1,21 @@
-// import AdminLayout from "@/components/admin/AdminLayout";
-
-// export const metadata = {
-//     title: "evercart - Admin",
-//     description: "evercart - Admin",
-// };
-
-// export default function RootAdminLayout({ children }) {
-
-//     return (
-//         <>
-//             <AdminLayout>
-//                 {children}
-//             </AdminLayout>
-//         </>
-//     );
-// }
-
-// "use client";
-
-// import AdminLayout from "@/components/admin/AdminLayout";
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-// import axios from "axios";
-
-// export const metadata = {
-//     title: "evercart - Admin",
-//     description: "evercart - Admin",
-// };
-
-// export default function RootAdminLayout({ children }) {
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const check = async () => {
-//       try {
-//         const res = await axios.get("http://localhost:8080/api/auth/me", {
-//           withCredentials: true,
-//         });
-
-//         if (res.data?.user?.role !== "seller") {
-//           router.replace("/admin/login");
-//         }
-//       } catch {
-//         router.replace("/admin/login");
-//       }
-//     };
-
-//     check();
-//   }, []);
-
-//   return <AdminLayout>{children}</AdminLayout>;
-// }
-
 "use client";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import AdminLayout from "@/components/admin/AdminLayout";
-import loading from "@/components/Loading";
+import Loading from "@/components/Loading"; 
 import axios from "axios";
 
 export default function RootAdminLayout({ children }) {
   const router = useRouter();
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
         const res = await axios.get(
-          //   "http://localhost:8080/api/auth/me",
           `${process.env.NEXT_PUBLIC_AUTH_SERVICE_API_URL}/api/auth/me`,
-          { withCredentials: true },
+          { withCredentials: true }
         );
 
         if (res.data?.user?.role !== "seller") {
@@ -78,16 +23,16 @@ export default function RootAdminLayout({ children }) {
           return;
         }
 
-        setLoading(false);
-      } catch {
+        setIsLoading(false);
+      } catch (error) {
         router.replace("/admin/login");
       }
     };
 
     checkAuth();
-  }, []);
+  }, [router]);
 
-  if (loading) return <Loading />;
+  if (isLoading) return <Loading />;
 
   return <AdminLayout>{children}</AdminLayout>;
 }
